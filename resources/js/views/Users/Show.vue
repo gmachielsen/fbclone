21b1770b15e373e515c4a2eee1ff5ed6a83eee1d
gmachielsen <g.machielsen@gmail.com>
2020-05-15 22:38:06 +0200

    usertest

---
+++ resources/js/views/Users/Show.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div class="w-100 h-64 overflow-hidden">
+            <img src="" alt="this is the user background image" class="object-cover w-full">
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "Show",
+
+    data: () => {
+        return {
+            user: null,
+            loading: true,
+        }
+    },
+
+    mounted() {
+        axios.get('/api/users/' + this.$route.params.userId)
+            .then(res => {
+                this.user = res.data;
+            })
+            .catch(error => {
+                console.log('Unable to fetch the user from the server.');
+            })
+            .finally(() => {
+                this.loading = false;
+            });
+
+
+            axios.get('/api/posts' + this.$route.params.userId)
+                .then(res => {
+                    this.posts = res.data;
+                    this.loading = false;
+                })
+                .catch(error => {
+                    console.log('Unable to fetch posts');
+                    this.loading = false;
+                });
+    }
+}
+</script>
+<style scoped>
+
+</style>
